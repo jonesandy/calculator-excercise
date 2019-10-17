9e826fa62f0a3b816b7b991def9e33cc6aafe427
Andy Jones <andytjones@icloud.com>
2019-10-17 23:06:12 +0100

    Third test written and RED.

--- spec/basic_maths_spec.rb
+++ spec/basic_maths_spec.rb
@@ -11,4 +11,9 @@ describe 'add' do
     calc = Arithmatic.new
     expect(calc.add(1,1)).to eq 2
   end
+
+  it 'should return 3 when passed 2 and 1' do
+    calc = Arithmatic.new
+    expect(calc.add(2,1)).to eq 3
+  end
 end
